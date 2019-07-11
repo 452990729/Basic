5152a53e9365634cc6a4da95db7774b7dc6858f8
452990729 <452990729@qq.com>
2019-07-11 10:29:23 +0800

    add MergeData.py

--- GeneIDTk.py
+++ GeneIDTk.py
@@ -64,9 +64,25 @@ def Anno(dict_gene, dict_ens, pd_data):
     pd_data.insert(2, 'Interpro', pd_anno)
     return pd_data
 
+def Switch(dict_gene, dict_ens, pd_data):
+    if pd_data.index[0].startswith('ENS'):
+        dict_in = dict_ens
+        lb = 'gene_name'
+    else:
+        dict_in = dict_gene
+        lb = 'gene_id'
+    list_tmp = []
+    for key in pd_data.index:
+        if key in dict_in:
+            list_tmp.append(dict_in[key].attr[lb])
+        else:
+            list_tmp.append('NotFind')
+    pd_data.index = list_tmp
+    return pd_data[pd_data.index != 'NotFind']
+
 def main():
     parser = argparse.ArgumentParser(description="Annotation Genes/Extract subtype")
-    parser.add_argument('method', help='the method used extract/anno', choices=['extract', 'anno'], nargs=1)
+    parser.add_argument('method', help='the method used extract/anno/switch', choices=['extract', 'anno', 'switch'], nargs=1)
     parser.add_argument('-m', help='input gene matrix, colomns one is Gene ids', required=True)
     parser.add_argument('-l', help='whether input gene matrix has header <<True>>', default=True)
     parser.add_argument('-t', help='extract data type', choices=['LncRNA', 'mRNA', 'sRNA', 'pseudogene', 'miRNA', 'rRNA', 'snoRNA'])
@@ -89,6 +105,9 @@ def main():
     elif argv['method'][0] == 'anno':
         pd_out = Anno(dict_gene, dict_ens, pd_data)
         pd_out.to_csv('AnnoMatrix.txt', sep='\t', header=True, index=True)
+    elif argv['method'][0] == 'switch':
+        pd_out = Switch(dict_gene, dict_ens, pd_data)
+        pd_out.to_csv('SwitchMatrix.txt', sep='\t', header=True, index=True)
 
 
 if __name__ == '__main__':
---
+++ MergeData.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python2
+
+
+import sys
+import re
+import argparse
+import pandas as pd
+
+
+def ReadData(file_in):
+    pd_data = pd.read_csv(file_in, sep='\t', header=0, index_col=0)
+    return pd_data
+
+def MergeData(pd1, pd2, method):
+    pd_out = pd1.merge(pd2, left_index=True, right_index=True, how=method)
+    return pd_out
+
+def MakeFinal(file_list, method):
+    pd_raw = ReadData(file_list[0])
+    for fl in file_list[1:]:
+        pd_data = ReadData(fl)
+        pd_raw = MergeData(pd_raw, pd_data, method)
+    return pd_raw
+
+def main():
+    parser = argparse.ArgumentParser(description="Merge data by columns")
+    parser.add_argument('-i', help='input matrix file, seperate by ,', required=True)
+    parser.add_argument('-m', help='the merge method <<inner>>', choices=['left', 'right', 'outer', 'inner'], default='inner')
+    argv=vars(parser.parse_args())
+    pd_out = MakeFinal(re.split(',', argv['i']), argv['m'])
+    pd_out.to_csv('MergedMatrix.txt', sep='\t', header=True, index=True)
+
+
+if __name__ == '__main__':
+    main()
