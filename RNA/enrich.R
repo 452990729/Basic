2cdeeb8df54148b63071792b0f04bbd9c2239ce3
452990729 <452990729@qq.com>
2019-08-15 10:02:18 +0800

    mod csript

--- RNA/enrich.R
+++ RNA/enrich.R
@@ -4,36 +4,36 @@ library("topGO")
 library("Rgraphviz")         
 library("pathview")                
 library("org.Hs.eg.db")
-argv=commandArgs(TRUE)
-outdir=argv[1]
+library("argparse")
+
+parser <- ArgumentParser(description='Run deseq2 by readscount')
+parser$add_argument('-f', help='the input gene file, col1 is gene, , requered')
+parser$add_argument('-out', help='output path <<.>>', default='./Enrich')
+parser$add_argument('-type', help='gene name type <<SYMBOL>>', default='SYMBOL')
+argv <- parser$parse_args()
+
+result=read.table(argv$f, sep="\t", header=F, quote="", comment="", check.names=F)
+
+outdir=argv$out
 if(file.exists(outdir)) {
     setwd(outdir)
 } else {
     dir.create(outdir)
     setwd(outdir)
 }
-print(1)
-if(file.exists(argv[2])) {
-    result=read.table(argv[2], sep="\t", header = F)
-} else {
-    result=read.table(file('stdin'), sep="\t", header = F)
-}
+
 genes=as.character(result$V1)
-if(length(argv)==3) {
-    genetype=argv[3]
-} else {
-    genetype="SYMBOL"
-}
+genetype=argv$type
+
 entrez_id=mapIds(x=org.Hs.eg.db,keys=genes,keytype=genetype,column ="ENTREZID")
-print(entrez_id)
 entrez_id=na.omit(entrez_id)
 erich.go.ALL=enrichGO(gene=entrez_id,OrgDb=org.Hs.eg.db,keyType="ENTREZID",ont="ALL",pvalueCutoff=0.05,qvalueCutoff=0.05)
-write.csv(summary(erich.go.ALL),paste(basename(outdir), ".G-enrich.csv", sep=""),row.names =F)
+write.table(summary(erich.go.ALL),paste(basename(outdir), ".G-enrich.txt", sep=""), row.names =F, sep = '\t', quote = F)
 pdf(paste(basename(outdir), ".G-enrich.pdf", sep=""),height = 10,width = 10)
 barplot(erich.go.ALL,drop=TRUE,showCategory = 25)
 dev.off()
 KEGG=enrichKEGG(gene=entrez_id,pvalueCutoff=0.05,qvalueCutoff=0.1)
-write.csv(summary(KEGG),paste(basename(outdir), ".K-enrich.csv", sep=""),row.names =F)
+write.table(summary(KEGG),paste(basename(outdir), ".K-enrich.txt", sep=""), row.names =F, sep = '\t', quote = F)
 pdf(paste(basename(outdir), ".K-enrich.pdf", sep=""), height = 10,width = 10)
 dotplot(KEGG,showCategory = 12) 
 dev.off()
